60ccbfa106256f92897ceaa4858e62c750eaead3
vernypv <37030303+vernypv@users.noreply.github.com>
2019-10-16 01:14:49 -0700

    Add files via upload

---
+++ wisdompets/adoptions/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Pet
+def home(request):
+    pets = Pet.objects.all()
+    return render(request, 'home.html', {'pets': pets})
+    
+    #return HttpResponse('<p>home view</p>')
+
+def pet_detail(request, id):
+    try:
+        pet = Pet.objects.get(id=id)
+    except Pet.DoesNoExist:
+        raise Http404('pet not found')
+    return render(request, 'pet_detail.html', {'pet': pet})
+    
+    #return HttpResponse('<p>pet_detail view with the id {}</p>'.format(id))
+
+
+
+
